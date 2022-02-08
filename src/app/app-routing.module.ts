d7e8c2bf843c84c44616572f9385e431ad85671f
Mohamed Gamal <62777579+MahamedRadwan@users.noreply.github.com>
2022-02-08 18:13:04 +0200

    1stCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'studentsData', component: StudentsComponent },
+  { path: 'coursesStats', component: CoursesComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { CoursesComponent } from './courses/courses.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    CoursesComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
